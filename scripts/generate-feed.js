55bb00865219ce1e0322d3b70ea0b5f13d9a2dda
Yusuke Nakamura <yusuke1994525@gmail.com>
2020-07-25 01:05:06 +0900

    [WIP]Generate feed.xml

--- scripts/generate-feed.js
+++ scripts/generate-feed.js
@@ -1,7 +1,63 @@
-const fs = require("fs");
-// const yaml = require("js-yaml");
+/* eslint-disable @typescript-eslint/no-var-requires, no-undef */
 
-;(async () => {
-    const xml = `<?xml version="1.0" encoding="UTF-8"?><sample>Sorry, wait a moment please.</sample>`
-    fs.writeFileSync('public/feed.xml', xml);
-})()
+const fs = require("fs");
+const path = require("path");
+const Podcast = require("podcast");
+const yaml = require("js-yaml");
+const fetch = require("node-fetch");
+
+(async () => {
+  const episodes = yaml.safeLoad(
+    fs.readFileSync(path.join(process.cwd(), "episodes.yaml"), "utf8")
+  );
+
+  const feed = new Podcast({
+    title: "unasuke.fm",
+    description: "description",
+    feed_url: `${process.env.DEPLOY_URL}/feed.xml`,
+    site_url: process.env.DEPLOY_URL,
+    image_url: `${process.env.DEPLOY_URL}/artwork.png`,
+    docs: process.env.DEPLOY_URL,
+    author: "unasuke",
+    language: "ja",
+    categories: ["Technology"],
+    ttl: "60",
+    itunesAuthor: "unasuke",
+    itunesSubtitle: "",
+    itunesSummary: "",
+    itunesOwner: { name: "unasuke", email: "yusuke1994525@gmail.com" },
+    itunesExplicit: false,
+    itunesCategory: [
+      {
+        text: "Entertainment",
+        subcats: [
+          {
+            text: "Television",
+          },
+        ],
+      },
+    ],
+    itunesImage: `${process.env.DEPLOY_URL}/artwork.png`,
+  });
+  for (const ep of episodes) {
+    // console.log(ep);
+    let length, type;
+    await fetch(ep.url, { method: "HEAD" })
+      .then((response) => {
+        length = response.headers.get("content-length");
+        type = response.headers.get("content-type").split("; ")[0];
+      })
+      .catch((e) => {
+        console.warn(e);
+      });
+    feed.addItem({
+      title: ep.title,
+      description: ep.description,
+      url: `${process.env.DEPLOY_URL}/ep/${ep.id}`,
+      date: ep.date,
+      enclosure: { url: ep.url, size: length.toString(), type: type },
+    });
+  }
+  const xml = feed.buildXml("  ");
+  fs.writeFileSync("public/feed.xml", xml);
+})();
