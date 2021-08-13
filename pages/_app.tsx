bcf9ef20a0b69ca925e78b3c12ab80515090e648
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-08-13 17:56:08 +0900

    Fix meta tag warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/global.css";
+import Head from "next/head";
 import { AppProps } from "next/app";
 
 export default function UnasukeFmApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
