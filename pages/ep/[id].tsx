ff6112cb762cec43d472218aa7d115a996c0f2fd
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-11-06 15:51:13 +0900

    Change unavatar url

--- pages/ep/[id].tsx
+++ pages/ep/[id].tsx
@@ -37,7 +37,7 @@ function Episode({ episode }) {
                     <li key={index} className={styles.guest}>
                       <a href={`https://twitter.com/${guest.twitter}`}>
                         <img
-                          src={`https://unavatar.now.sh/github/${guest.github}`}
+                          src={`https://unavatar.io/github/${guest.github}`}
                           className={styles.avatar}
                           alt={guest.twitter}
                         />
