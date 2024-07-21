517324da85b7b5511361f816fceb08cfce287fb8
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-07-22 01:36:00 +0900

    Use google analytics through @next/third-parties

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/global.css";
 import Head from "next/head";
 import { AppProps } from "next/app";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function UnasukeFmApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ export default function UnasukeFmApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
     </>
   );
 }
