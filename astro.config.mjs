be1b3947a562817cd0c7c30d81230c9f30f0a91a
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-12-13 01:27:18 +0900

    Configure astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://unasuke.fm",
+  output: "static",
+  compressHTML: false,
+});
